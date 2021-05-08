73ce8a815f0e680637e4afc7c14355c664ff0495
Klaidas <klaidas.predeinas1@gmail.com>
2021-05-08 18:09:37 +0100

    added fs

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const app = express();
 const http = require("http").createServer(app);
+const fs = require("fs");
 const {fromFS} = require("./FS-endpoints");
 const bodyParser = require("body-parser")
 const cors = require('cors');
