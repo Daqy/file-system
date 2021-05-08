d9dddff3207a4c073e2b40798196ae8cadac437b
Klaidas <klaidas.predeinas1@gmail.com>
2021-05-08 18:17:13 +0100

    config update

--- database-config.js
+++ database-config.js
@@ -1,9 +1,7 @@
 const config = {
-  user: "postgres",
   host: "localhost",
   database: "file-system",
-  password: "n6hxVgtztchh",
-  port: 5432,
+  port: 5432
 }
 
 module.exports.config = config;
